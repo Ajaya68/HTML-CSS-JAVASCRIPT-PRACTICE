46cd63c1f17277cb67df3e8258cd0feeb33836f2
Ajaya68 <ajayafullstackdev@gmail.com>
2026-08-08 13:01:56 +0530

    understanding async and await method and fetching api

--- src/script/Promises.js
+++ src/script/Promises.js
@@ -13,19 +13,41 @@
 // promiseValue.then((value) => console.log(value));
 // promiseValue.catch((error) => console.log(error));
 
-let fetchProduct = () => {
-  let data = fetch("https://fakestoreapi.com/products");
-  console.log(data);
-  return data;
-};
+// let fetchProduct = () => {
+//   let data = fetch("https://fakestoreapi.com/products");
+//   console.log(data);
+//   return data;
+// };
 
-// fetchProduct();
-fetchProduct()
-  .then((value) => {
-    return value.json();
-  })
-  .then((value) =>
-    value.map((product) => {
+// // fetchProduct();
+// fetchProduct()
+//   .then((value) => {
+//     return value.json();
+//   })
+//   .then((value) =>
+//     value.map((product) => {
+//       let container = document.createElement("article");
+//       let button = document.createElement("button");
+//       let para = document.createElement("p");
+//       let image = document.createElement("img");
+
+//       para.innerText = product.title;
+//       button.innerText = "Add to cart";
+//       image.src = product.image;
+
+//       container.append(image, para, button);
+//       document.querySelector("body").appendChild(container);
+//     }),
+//   );
+// fetchProduct().catch((error) => console.log(error));
+
+let fetchProduct = async () => {
+  try {
+    let response = await fetch("https://fakestoreapi.com/products");
+    let data = await response.json();
+    // console.log(data);
+
+    data.map((product) => {
       let container = document.createElement("article");
       let button = document.createElement("button");
       let para = document.createElement("p");
@@ -37,6 +59,10 @@ fetchProduct()
 
       container.append(image, para, button);
       document.querySelector("body").appendChild(container);
-    }),
-  );
-fetchProduct().catch((error) => console.log(error));
+    });
+  } catch (error) {
+    console.log(error)
+  }
+};
+
+fetchProduct();
